c3dcbd4856d30b030bb4c70a55e0abf3bd4e4f10
Addison Hill <adddison.hill@gmail.com>
2020-10-19 14:43:11 -0500

    transfered to windows

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,17 +1,23 @@
-import { Input } from 'semantic-ui-react'
+import React from "react";
+import { Input } from "semantic-ui-react";
 
 function AddProductToCart() {
-  return <Input 
-    type="number"
-    min="1"
-    placeholder="Quantity"
-    value="1"
-    action={{
-      color: "orange",
-      content: "Add to Cart",
-      icon: "plus cart"
-    }}
-  />
+  const [quantity, setQuantity] = React.useState(1);
+
+  return (
+    <Input
+      type="number"
+      min="1"
+      placeholder="Quantity"
+      value={quantity}
+      onChange={(event) => setQuantity(Number(event.target.value))}
+      action={{
+        color: "orange",
+        content: "Add to Cart",
+        icon: "plus cart",
+      }}
+    />
+  );
 }
 
 export default AddProductToCart;
